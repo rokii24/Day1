1fcd5cb0a81dddbd13fb6e885ec1aa7000ab3e34
shroukmohamed 30203242200708 <shroukmohamed896_sd@fcis.bsu.edu.eg>
2024-02-29 17:20:37 +0200

    Day1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from 'src/components/navBar/navBar.component';
+import { FooterComponent } from 'src/components/footer/footer.component';
+import { AsideComponent } from 'src/components/aside/aside.component';
+import { HomeComponent } from 'src/components/home/home.component';
+import { SliderComponent } from 'src/components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    AsideComponent,
+    HomeComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/components/navBar/navBar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+    selector:'app-navBar',
+    templateUrl:'./navBar.component.html',
+    styleUrls:[
+        './navBar.component.css'
+    ]
+})
+export class NavBarComponent{
+
+}
